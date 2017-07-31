83bcd1c61c1b5bcd5fd237e235c6d857fa12bd75
Rui Paiva Branco <email@ruipaivabranco.com>
2017-07-31 15:59:12 +0100

    First commit
    Inital Falsk app with examples about routing and queries using SQL
    Alchemy.

---
+++ project.py
@@ -0,0 +1,81 @@
+''' first, I imported this Flask class from the Flask library. '''
+from flask import Flask
+''' next, I create an instance of this class with the name
+of running application. Anytime we run an application in Python,
+a special variable called name gets defined for the application
+and all of the imports it uses.'''
+app = Flask(__name__)
+
+''' Add CRUD functionality  '''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+''' I have this thing that looks like a function, but starts
+with an @ symbol. This is called a decorator in Python. This
+decorator essentially wraps our function inside app.route
+function that Flask has already created.'''
+
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+	'''I will change my restaurantMenu function to get the first
+	restaurant in data base  '''
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+	''' list of all itens of menu and stored in a string called output '''
+	items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+	output = ''
+	for i in items:
+		output += i.name
+		output += '</br>'
+		output += i.price
+		output += '</br>'
+		output += i.description	
+		output += '</br>'
+		output += '</br>'
+	''' return the string for user could see the items of restaurant'''
+	return output
+
+# Task 1: Create route for newMenuItem function here
+
+
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+
+
+
+
+
+''' the application run by the Python interpreter gets a name
+variable set to __main__.
+The if statment here makes sure the server only runs if the
+script is executed directly from the Python interpreter, and
+not used as an imported module.
+So basically, this line of code says, if you're executing
+me with the Python interpreter, then do this '''
+if __name__ == '__main__':
+	''' If you enable debug support the server will reload
+	itself each time it notices a code change. It will also
+	provide you a helpful debugger in the browser if things 
+	go wrong'''
+	app.debug = True
+	''' I use the run function to run the local server with
+	our application - (host = '0.0.0.0', port = 5000) this
+	tells the web server on my vagran machine to listen on 
+	all public IP addresses'''
+	app.run(host = '0.0.0.0', port = 5000)
